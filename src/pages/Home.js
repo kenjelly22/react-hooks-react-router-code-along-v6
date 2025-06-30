8c6b1c0e8113da1c6a70787873193ae2672e4420
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-29 22:15:56 -0400

    Complete code along

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,33 @@
-import { useState, useEffect } from "react";
-import UserCard from "../components/UserCard";
+// Home.js
+import {useState, useEffect} from "react"
+import UserCard from "../components/UserCard"
+import NavBar from "../components/NavBar"
 
 function Home() {
   const [users, setUsers] = useState([])
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>
-  });
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  const userList = users.map((user) => {
+    return <UserCard key={user.id} user={user} />
+  })
 
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
         {userList}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
